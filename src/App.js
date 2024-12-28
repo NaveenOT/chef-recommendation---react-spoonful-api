c0069d1843600b1b8b3c7a3407674df54afee522
Your Name <naveen.ottilingam@gmail.com>
2024-12-28 10:41:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+function MainComponent(){
+    return(
+      <h1>Hello World</h1>
+    )
+}
+function App() {
+  return (
+    <div className="App">
+      <img src='logo512.png' width = "40px"/>
+      <h1>Fun Facts About React</h1>
+      <ul className = 'list'>
+        <li>created in 2013</li>
+        <li>JAVASCRIPT Framework</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
